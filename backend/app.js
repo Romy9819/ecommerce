992790b5e39ef9c69964e004fa720e17088f8097
Romy9819 <romymathew732@gmail.com>
2022-08-10 22:08:03 +0530

    romy work 3:15:11

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
 const errorMiddleware = require('./middleware/error');
+const cookieParser = require("cookie-parser")
 
+// Route Imports
 const productsRoute = require('./routes/productsRoute');
+const userRoute = require('./routes/userRoutes')
 
 app.use(express.json());
+app.use(cookieParser())
 
 app.use('/api/v1',productsRoute);
-
+app.use('/api/v1', userRoute)
 // Middleware for errors
 app.use(errorMiddleware);
 
--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -56,6 +56,12 @@ const productSchema = mongoose.Schema({
             required: true
         }
     }],
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+        required: "true"
+    },
+    
     createdAt: {
         type: Date,
         default: Date.now
